8cbfccdd7d9c8281529d2865b54426039446d4c4
Brian <github.holtz@mail.brnlee.com>
2024-07-15 22:14:39 -0400

    Return next state instead of directly setting self.state

--- aniflow/__main__.py
+++ aniflow/__main__.py
@@ -38,21 +38,21 @@ class AniFlow:
                 match (self.state):
                     case State.SELECT_EPISODE:
                         self.reset()
-                        self.select_episode()
+                        self.state = self.select_episode()
                     case State.PLAY_VIDEO:
-                        self.play_video()
+                        self.state = self.play_video()
                     case State.GET_AND_SET_EPISODE_METADATA:
-                        self.get_and_set_episode_metadata()
+                        self.state = self.get_and_set_episode_metadata()
                     case State.OPEN_REDDIT_DISCUSSION:
-                        self.open_reddit_discussion()
+                        self.state = self.open_reddit_discussion()
                     case State.AUTH_ANILIST:
-                        self.auth_anilist()
+                        self.state = self.auth_anilist()
                     case State.UPDATE_ANILIST:
-                        self.update_anilist()
+                        self.state = self.update_anilist()
                     case State.OPEN_ANILIST:
-                        self.open_anilist()
+                        self.state = self.open_anilist()
                     case State.DELETE_EPISODE:
-                        self.delete_episode()
+                        self.state = self.delete_episode()
                     case _:
                         self.state = State.SELECT_EPISODE
         except KeyboardInterrupt:
@@ -64,30 +64,26 @@ class AniFlow:
 
     def select_episode(self):
         reload_episodes_choice = "[Reload Episodes]"
-
         choices = [reload_episodes_choice] + self.qbittorrent.get_episodes()
         choice = prompt.list("Select an episode", choices)
         if choice is reload_episodes_choice:
-            return
+            return State.SELECT_EPISODE
         else:
             self.episode_choice = choice
-            self.state = State.PLAY_VIDEO
+            return State.PLAY_VIDEO
 
     def play_video(self):
         play_video = prompt.confirm("Play video?")
         if play_video:
             os.startfile(self.episode_choice.path)
-        self.state = State.GET_AND_SET_EPISODE_METADATA
+        return State.GET_AND_SET_EPISODE_METADATA
 
     def get_and_set_episode_metadata(self):
         self.anilist.find_and_set_data(self.episode_choice)
-        self.state = State.OPEN_REDDIT_DISCUSSION
+        return State.OPEN_REDDIT_DISCUSSION
 
     def open_reddit_discussion(self):
-        self.state = State.AUTH_ANILIST
-
         reddit_thread = self.reddit.get_discussion_thread(self.episode_choice)
-
         open_reddit_discussion = prompt.confirm("Open r/anime discussion thread?")
         if open_reddit_discussion:
             if reddit_thread:
@@ -96,30 +92,24 @@ class AniFlow:
             else:
                 url = self.reddit.get_generic_search_url(self.episode_choice)
             webbrowser.open_new(url)
+        return State.AUTH_ANILIST
 
     def auth_anilist(self):
-        self.state = State.UPDATE_ANILIST
+        if self.anilist.should_auth():
+            proceed = prompt.confirm(
+                "AniList requires your authorization in order to update your anime list. Proceed?"
+            )
+            if not proceed:
+                return State.OPEN_ANILIST
 
-        if not self.anilist.should_auth():
-            return
-
-        proceed = prompt.confirm(
-            "AniList requires your authorization in order to update your anime list. Proceed?"
-        )
-        if not proceed:
-            self.state = State.OPEN_ANILIST
-            return
-
-        self.anilist.open_authorization_page()
-
-        access_token = prompt.password("Paste the token provided by AniList")
-        self.anilist.set_access_token(access_token)
+            self.anilist.open_authorization_page()
+            access_token = prompt.password("Paste the token provided by AniList")
+            self.anilist.set_access_token(access_token)
+        return State.UPDATE_ANILIST
 
     def update_anilist(self):
-        self.state = State.OPEN_ANILIST
-
         if not self.episode_choice.anilist_entry:
-            return
+            return State.DELETE_EPISODE
 
         update_anilist = prompt.confirm(
             f'Update progress on AniList for "{self.episode_choice.anilist_entry.titles[0]}"?'
@@ -127,24 +117,21 @@ class AniFlow:
         if update_anilist:
             encountered_auth_error = self.anilist.update_entry(self.episode_choice)
             if encountered_auth_error:
-                self.state = State.AUTH_ANILIST
+                return State.AUTH_ANILIST
+        return State.OPEN_ANILIST
 
     def open_anilist(self):
-        self.state = State.DELETE_EPISODE
-
-        if not self.episode_choice.is_last_episode():
-            return
-
-        open_anilist = prompt.confirm("Open AniList page for the anime?")
-        if open_anilist:
-            webbrowser.open_new(self.episode_choice.anilist_entry.url)
+        if self.episode_choice.is_last_episode():
+            open_anilist = prompt.confirm("Open AniList page for the anime?")
+            if open_anilist:
+                webbrowser.open_new(self.episode_choice.anilist_entry.url)
+        return State.DELETE_EPISODE
 
     def delete_episode(self):
-        self.state = State.SELECT_EPISODE
-
         delete_episode = prompt.confirm("Delete episode?", default=False)
         if delete_episode:
             self.qbittorrent.delete(self.episode_choice)
+        return State.SELECT_EPISODE
 
 
 if __name__ == "__main__":
